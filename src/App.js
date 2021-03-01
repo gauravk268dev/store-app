9a7d13f51382b17fdebe495f249d80bf4c21a151
Gaurav Kumar <gauravk268.dev@gmail.com>
2021-03-01 21:45:45 +0530

    - added pages about, cart, contact - added dummy contact detail in contact and feedback form

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import About from './components/About';
-import Shop from './components/Shop';
+import Cart from './components/Cart';
 import Contact from './components/Contact';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <div className="App jumbotron">
+      <div className="App">
         <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/about" component={About} />
-          <Route path="/shop" component={Shop} />
+          <Route path="/cart" component={Cart} />
           <Route path="/contact"component={Contact} />
         </Switch>
       </div>
--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import about from './data/about';
+import "./style/about.css";
 
 const About = () => {
   return (
-    <div className="about-page">
-      <h1>About Page</h1>
+    <div className="about-section">
+      <div className="about about-title" ><h1>About Us</h1></div>
+      <div className="about about-msg container"><p>{about.p2}</p></div>
+      <div className="about about-msg container"><p>{about.p3}</p></div>
+      <div className="about about-msg container"><p>{about.p1}</p></div>
     </div>
   );
 }
